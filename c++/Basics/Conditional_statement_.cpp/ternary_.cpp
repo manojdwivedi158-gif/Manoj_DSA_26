1e6a803de12e5cc874c7071696de9d244745bbed
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-02-22 13:31:18 +0530

    practice_question

---
+++ c++/Basics/Conditional_statement_.cpp/ternary_.cpp
@@ -0,0 +1,13 @@
+//(condition) ? (if true) : (if false)
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter a no.";
+    cin>> x;
+
+    // (condition) ? if true : if false
+
+    (x%2==0) ? cout<<"even no." : cout<<"odd";
+}
---
+++ c++/practice.cpp/bas_conditional.cpp
@@ -0,0 +1,11 @@
+// Predict the output
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x=3,y,z;
+    y=x=10;
+    z=x<10;   // 10-10 = 0
+
+    cout<<x<<" "<<y<<" "<<z;
+}
