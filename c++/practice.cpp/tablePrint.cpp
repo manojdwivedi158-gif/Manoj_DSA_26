be8dabacba5b85ee8913697fa97a51820d231f53
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-02-25 04:08:37 +0530

    practice

---
+++ c++/practice.cpp/tablePrint.cpp
@@ -0,0 +1,22 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i;
+    
+//     for(int i=15;i<=150;i++){
+//         if(i%15==0){
+//             cout<<i<<endl;
+//         }
+//     }
+// }
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=15;i<=150;){
+        cout<<i<<endl;
+    }
+}
