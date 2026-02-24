4eaa712ddf2bc9367862777e0dcdb8b16e5b8954
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-02-24 09:08:39 +0530

    loopInput

--- c++/loops.cpp/evenNums_.cpp
+++ c++/loops.cpp/evenNums_.cpp
@@ -1,10 +1,22 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1;i<=100;i++){
+//         if(i%2==0){
+//             cout<<i<<endl;
+//         }
+        
+//     }
+// }
+
+
 #include<iostream>
 using namespace std;
 int main(){
-    for(int i=1;i<=100;i++){
-        if(i%2==0){
-            cout<<i<<endl;
-        }
-        
+    int i;
+    cout<<"enter your number to start:";
+    cin>>i;
+    for(i;i<=100;i++){
+        cout<<i<<endl;
     }
 }
