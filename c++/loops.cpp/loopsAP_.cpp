b78bc1441ba6cdef9e13eb26cd3447081b712bd0
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-02-27 23:55:59 +0530

    loops

--- c++/loops.cpp/loopsAP_.cpp
+++ c++/loops.cpp/loopsAP_.cpp
@@ -1,16 +1,32 @@
-#include<iostream>
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+
+//     int x;
+//     cout<<"enter n.";
+//     cin>>x;
+//     int a=3;
+//     for(int i=2;i<=x;i++){
+//         cout<<a<<" ";
+//         a=a+3;
+//     }
+
+
+// }
+
+
+
+//  QUESTION: print ...8,11,14,17........;
+
+#include<iostream>              // formula .An==. a+(n-1)d
 using namespace std;
 int main(){
-
-
-    int x;
-    cout<<"enter n.";
-    cin>>x;
-    int a=3;
-    for(int i=2;i<=x;i++){
-        cout<<a<<" ";
-        a=a+3;
+    int n;
+    cin>>n;
+    for(int i=8;i<=3*n+5;i+=3){
+        cout<<i<<" ";
     }
-
-
 }
---
+++ c++/loops.cpp/loops_ap.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"enter your first no.";
+    cin>>x;
+    for(int i=4;i<=)
+}
