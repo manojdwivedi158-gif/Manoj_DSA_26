67b67f03d20167e57e52e59f5ab66435d7c06853
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-02 08:22:22 +0530

    loops

--- c++/loops.cpp/2_loop_user.cpp
+++ c++/loops.cpp/2_loop_user.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 using namespace std;
+int main(){
+    int n,f;
+    cout<<"enter your number to print factor:";
+    cin>>n;
+    for(int i=1;i<=n/2;i++){
+        if(n%i==0){
+        f=i;
+        }
+    }
+    cout<<f<<" ";
+}
 
 
 
