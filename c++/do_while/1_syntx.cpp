62dfd9ab340ef866b1e7cf19914fa55f3472036f
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-05 13:09:59 +0530

    while_loop

---
+++ c++/do_while/1_syntx.cpp
@@ -0,0 +1,21 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int i=1;
+// do{
+//     cout<<i<<' ';
+//     i++;
+// } while(i<=10);
+// }
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i=11;              //  It will always once run weather contition is true or not:
+    do{
+        cout<<i<<" ";
+        i++;
+    }
+    while(i<=10);
+}
