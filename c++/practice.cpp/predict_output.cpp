bb0e6c7f764c271246177692b2070374ace59b37
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-05 19:35:33 +0530

    practice

---
+++ c++/practice.cpp/predict_output.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){                 //  THIS  WILL PRINT INFINITE TIMES :
+    while('a'<'b'){
+        cout<<"malyalam is palindrome "<<endl;  
+    }
+}
