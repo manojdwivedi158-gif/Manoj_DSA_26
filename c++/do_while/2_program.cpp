67c320bbd80afdff0992cf0bd490e4d613af7b10
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-05 19:24:52 +0530

    loops

---
+++ c++/do_while/2_program.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=0;
+    do{
+        cout<<i<<endl;
+        i++;
+    }
+    while(i<11);
+}
+
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+// int i = 0;
+// do {
+// cout << i << endl;
+// i++;
+// } while (i <= 21);
+// return 0;
+// }
