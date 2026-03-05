56061408e4ae406ff83bc19d2a4f917072a32f36
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-05 19:52:21 +0530

    practice

--- c++/practice.cpp/whilePredict.cpp
+++ c++/practice.cpp/whilePredict.cpp
@@ -1,13 +1,3 @@
-// #include<iostream>
-// using namespace std;
-// int main(){                 //  THIS  WILL PRINT INFINITE TIMES :
-//     while('a'<'b'){
-//         cout<<"malyalam is palindrome "<<endl;  
-//     }
-//  }
-
-
-
 #include<iostream>
 using namespace std;
 int main(){
