62678cdc2788fb99fb80f8e9de7d0702ec1545d4
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-05 10:20:57 +0530

    continue_statement

---
+++ c++/continue_statement/1_conti_.cpp
@@ -0,0 +1,12 @@
+
+
+  //   CONTINUEE STATEMENT USED TO SKIP A PARTICULAR ROUND:
+
+  #include<iostream>
+  using namespace std;
+  int main(){
+        int n;
+        for(n=1;n<21;n++){
+            if(n!=2 && n!=5) cout<<n<<" ";
+        }
+  }
