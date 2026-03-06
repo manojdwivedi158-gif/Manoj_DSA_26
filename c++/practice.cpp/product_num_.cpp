59f4e4d6e59a8d2b6d68396cbd9154cd76c1647a
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-06 15:24:56 +0530

    while

--- c++/practice.cpp/product_num_.cpp
+++ c++/practice.cpp/product_num_.cpp
@@ -4,4 +4,15 @@ int main(){
     int n,pr=1;
     cout<<"enter your number:";
     cin>>n;
+
+    while(n!=0){
+        int p = n%10;
+        n = n/10;
+        pr *= p;
+
+    }
+    cout<<"the product of digits given numbers: "<<pr;
+    return 0;
+
+
 }
