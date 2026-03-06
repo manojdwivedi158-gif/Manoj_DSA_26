a04eaf6e7ca7c23e12eb88ce3723c42c3d786a64
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-06 19:39:53 +0530

    practice2

---
+++ c++/practice_2/sum_of_digit.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,n,sum=0;
+    cout<<"enter your number:";
+    cin>>i;
+    
+    while(i!=0){
+        n=i%10;
+        i=i/10;
+        sum=sum+n;
+    }
+    cout<<sum;
+}
