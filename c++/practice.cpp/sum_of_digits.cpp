fb8f5ad8f2263978f7c4ece9f204a3e0db33aaf8
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-06 12:09:53 +0530

    practice

--- c++/practice.cpp/sum_of_digits.cpp
+++ c++/practice.cpp/sum_of_digits.cpp
@@ -1,14 +1,29 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     int sum=0;
+//     cout<<"enter your number:";
+//     cin>>n;
+//     while(n!=0){
+//         int ld=n%10;
+//         n=n/10;
+//         sum+=ld;
+//     }
+//     cout<<sum<<endl;
+// }
+
+
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    int sum=0;
+    int n,sum=0;
     cout<<"enter your number:";
     cin>>n;
     while(n!=0){
-        int ld=n%10;
-        n=n/10;
-        sum+=ld;
+        int lastdigit = n%10;
+        n /=10;
+        sum+=lastdigit;
     }
-    cout<<sum<<endl;
+    cout<<sum;
 }
