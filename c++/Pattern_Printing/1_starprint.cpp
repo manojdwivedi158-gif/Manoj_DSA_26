bc3574bf5e18bb23a2161955c4bbfaf25b8668d7
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-09 15:31:14 +0530

    practice2.0

---
+++ c++/Pattern_Printing/1_starprint.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter your * cube's length:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<"****"<<endl;
+    }
+}
