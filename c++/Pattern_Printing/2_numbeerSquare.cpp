0d9c5fd43840f94c926947a3c85495baa901d019
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-10 15:46:51 +0530

    pattern_printing

--- c++/Pattern_Printing/2_numbeerSquare.cpp
+++ c++/Pattern_Printing/2_numbeerSquare.cpp
@@ -4,20 +4,37 @@
 //                  1234
 
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter your number:";
+//     cin>>n;
+    
+//         for(int i=1;i<=n;i++){
+//             for(int m=1;m<=n;m++){
+//             cout<<m<<" ";
+//             }
+//             cout<<endl;
+
+//         }
+//         cout<<endl;
+    
+// }
+
+
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<<"enter your number:";
+    int n,m;
+    cout<<"enter your number of rows:";
     cin>>n;
-    
-        for(int i=1;i<=n;i++){
-            for(int m=1;m<=n;m++){
-            cout<<m<<" ";
-            }
-            cout<<endl;
-
+     
+   
+    for(int i=n;i>=1;i--){
+        for(int j=i;j>=1;j--){
+            cout<<j<<" ";
         }
         cout<<endl;
-    
+    }
 }
