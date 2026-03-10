75512421526c5ac9fa3b5c5aacf3af47f72ae81b
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-10 18:18:16 +0530

    pattern_printing

---
+++ c++/Pattern_Printing/numAlphabet_triangle.cpp
@@ -0,0 +1,30 @@
+
+
+//   enter your no.5
+//   1 
+//   A B
+//   1 2 3
+//   A B C D
+//   1 2 3 4 5
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter your no.";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if(i%2!=0){
+                cout<<j<<" ";
+            }
+            else{
+                cout<<char(j+64)<<" ";
+            }
+            
+        }
+        cout<<endl;
+    }
+}
