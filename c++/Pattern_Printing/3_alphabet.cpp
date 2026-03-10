0bc44754d20bacb74e45b472380c78e486910563
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-10 13:03:08 +0530

    pattern_printing

---
+++ c++/Pattern_Printing/3_alphabet.cpp
@@ -0,0 +1,28 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter your number:";
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<char(i+96)<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"enter your number:";
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<char(j+64)<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
