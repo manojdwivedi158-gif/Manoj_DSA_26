63768bf6de4a3b51b61e894a5dc29bde6433d2c9
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-10 11:11:47 +0530

    pattern_printing

--- c++/Pattern_Printing/input_rowcoloumn.cpp
+++ c++/Pattern_Printing/input_rowcoloumn.cpp
@@ -1,15 +1,35 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n,m;
+//     cout<<"enter thr no. of rows:";
+//     cin>>n;
+//     cout<<"enter thr no. of coloumns:";
+//     cin>>m;
+//     for(int i=1;i<=n;i++){
+//         for(int i=1;i<=m;i++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
 #include<iostream>
 using namespace std;
 int main(){
     int n,m;
-    cout<<"enter thr no. of rows:";
+    cout<<"enter your number of rows:";
     cin>>n;
-    cout<<"enter thr no. of coloumns:";
+    cout<<"enter your number of coloumns:";
     cin>>m;
+
     for(int i=1;i<=n;i++){
-        for(int i=1;i<=m;i++){
-            cout<<"* ";
+        for(int j=1;j<=m;j++){
+            cout<<" *";
         }
         cout<<endl;
     }
+
+
 }
