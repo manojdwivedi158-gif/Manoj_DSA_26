1f73ba77cd33ffe8945a1321517d9c2838c042db
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-11 12:40:20 +0530

    pattern_printing

--- c++/Pattern_Printing/3_alphabet.cpp
+++ c++/Pattern_Printing/3_alphabet.cpp
@@ -1,17 +1,9 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int n;
-//     cout<<"enter your number:";
-//     cin>>n;
-//     for(int i=1;i<=n;i++){
-//         for(int j=1;j<=n;j++){
-//             cout<<char(i+96)<<" ";
-//         }
-//         cout<<endl;
-//     }
-// }
-
+//  enter your number:5
+//  a a a a a 
+//  b b b b b
+//  c c c c c 
+//  d d d d d
+//  e e e e e
 
 #include<iostream>
 using namespace std;
@@ -21,8 +13,31 @@ int main(){
     cin>>n;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            cout<<char(j+95)<<" ";
+            cout<<char(i+96)<<" ";
         }
         cout<<endl;
     }
 }
+
+
+//  #include<iostream>
+//  using namespace std;
+//  int main(){
+//      int n;
+//      cout<<"enter your number:";
+//      cin>>n;
+//      for(int i=1;i<=n;i++){
+//          for(int j=1;j<=n;j++){
+//              cout<<char(j+96)<<" ";
+//          }
+//          cout<<endl;
+//      }
+//  }
+
+//  enter your number:5
+
+//  a b c d e 
+//  a b c d e
+//  a b c d e 
+//  a b c d e
+//  a b c d e
