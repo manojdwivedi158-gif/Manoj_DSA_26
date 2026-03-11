8dd45f9277112040c93cab5622edbc02e74ec3d7
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-11 23:37:01 +0530

    pattern_printing

---
+++ c++/Pattern_Printing/starplusTriangle.cpp
@@ -0,0 +1,29 @@
+
+
+//   enter your no.5
+//       * 
+//       *
+//   * * * * *
+//       *
+//       * 
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+     int n;
+     cout<<"enter your no.";
+     cin>>n;
+     
+     for(int i=1;i<=n;i++){
+          for(int j=1;j<=n;j++){
+            
+               if(i==(n/2)+1 || j==(n/2)+3) cout<<"* ";
+               else cout<<" ";
+           
+          }cout<<endl;
+     }
+}
+
