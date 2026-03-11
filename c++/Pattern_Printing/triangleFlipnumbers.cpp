89c6ba669b218be8f0476c5ecbab0b16713b2cd0
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-11 10:28:12 +0530

    pattern_printing

---
+++ c++/Pattern_Printing/triangleFlipnumbers.cpp
@@ -0,0 +1,28 @@
+
+
+//  enter your name:7
+//  1 2 3 4 5 6 7 
+//  1 2 3 4 5 6
+//  1 2 3 4 5
+//  1 2 3 4
+//  1 2 3
+//  1 2
+//  1
+
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter your name:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n+1-i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
