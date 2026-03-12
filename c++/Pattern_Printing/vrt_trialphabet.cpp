88da256e49aa3fe111010604fd8db022ce0f539d
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-13 00:47:23 +0530

    pattern_printing

---
+++ c++/Pattern_Printing/vrt_trialphabet.cpp
@@ -0,0 +1,25 @@
+
+
+//  enter your no:5
+//          A 
+//        A B
+//      A B C
+//    A B C D
+//  A B C D E
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter your no:";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++)
+         cout<<"  ";
+         for(int j=1;j<=i;j++){
+            cout<<char(j+64)<<" ";
+         } cout<<endl;
+    }
+}
