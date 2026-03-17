730793ffd9d78f58de4ef6488fc5dbca833c1df6
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-17 16:52:03 +0530

    function

---
+++ c++/functions/1_basicprgm.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void greet(){
+    cout<<"good morning"<<endl;
+    cout<<"How are you?"<<endl;
+}
+int main(){
+    greet();
+    greet();
+    cout<<"hello"<<endl;
+    greet();
+}
