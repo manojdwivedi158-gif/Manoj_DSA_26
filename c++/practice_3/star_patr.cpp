f85ee0efc661d739def41dadc3dbb02b2b6690c5
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-17 16:25:38 +0530

    practice3

--- c++/practice_3/star_patr.cpp
+++ c++/practice_3/star_patr.cpp
@@ -14,7 +14,7 @@ int main(){
     cin>>n;
 
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
+        for(int j=1;j>=n-i;j++){
             cout<<" ";
         }
         for(int j=1;j<=n;j++){
--- c++/practice_3/verticallystartri.cpp
+++ c++/practice_3/verticallystartri.cpp
@@ -16,10 +16,10 @@ int main(){
     cin>>n;
 
     for(int i=1;i<=n;i++){
-        for(int j=1;j<=i-1;j++){
+        for(int j=1;j<=n-i;j++){
             cout<<"  ";
         }
-        for(int j=1;j<=n-i+1;j++){
+        for(int j=1;j<=i*2-1;j++){
             cout<<"* ";
         }
        
