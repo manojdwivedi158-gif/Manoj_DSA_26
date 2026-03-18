ddd125e5735e0a0af00cc4a14b4ca982b9517941
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-18 23:53:00 +0530

    function

---
+++ c++/functions/2_basic.cpp
@@ -0,0 +1,17 @@
+// aman
+// hello brother how are you
+//  hello manoj hello brother how are you#include<iostream>
+
+#include<iostream>
+using namespace std;
+void name(){
+   cout<<"hello brother ";
+   cout<<"how are you"<<endl;
+}
+int main(){
+    cout<<"aman"<<endl;
+    name();
+    cout<<" hello manoj ";
+    name();
+    return 0;
+}
