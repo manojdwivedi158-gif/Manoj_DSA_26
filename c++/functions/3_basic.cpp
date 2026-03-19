da168462a07b15bb1bcb0936aabedad5d5469413
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-19 16:18:27 +0530

    function

--- c++/functions/3_basic.cpp
+++ c++/functions/3_basic.cpp
@@ -1,10 +1,23 @@
 #include<iostream>
 using namespace std;
+void india(){
+  cout<<"welcome to india "<<endl;
+}
+void usa(){
+  cout<<"welcome to USA "<<endl;
+}
+void russia(){
+  cout<<" you are in russia:";
+}
 
-  void greet(){
-    cout<<" nice to you ";
-  }
-  int main(){
-    cout<<"hello sir";
-    greet();
-  }
+int main(){
+  cout<<"Russia"<<endl;
+  russia();cout<<endl<<endl;
+  cout<<"India ";cout<<endl;
+  india();cout<<endl;
+
+  cout<<"usa";cout<<endl;
+  usa();
+  cout<<"jai hind";
+
+}
