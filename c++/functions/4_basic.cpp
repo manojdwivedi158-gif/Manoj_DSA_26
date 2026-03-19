afac63acf2e8b837e56e97fc016625214c4afddf
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-19 18:05:07 +0530

    function

---
+++ c++/functions/4_basic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int india(){
+    cout<<" welcome to the country "<<endl;
+}
+int main(){
+    char a;
+    cout<<"enter the country you are going to visit :";
+    cin>>a;
+    
+    cout<<endl;
+    india();
+    
+}
