60c48706e9b7833b2ed37219ee460bb6a3ad0137
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-22 23:11:38 +0530

    function

---
+++ c++/functions/9_basic.cpp
@@ -0,0 +1,21 @@
+// #include<iostream>
+// using namespace std;
+
+// int multiplication(int a,int b){
+//     return a*b;
+// }
+
+// int main(){
+//     int a,b;
+
+// }.
+
+
+#include<iostream>
+using namespace std;
+int division(int a,int b){
+    return a/b;
+}
+int main(){
+    cout<<division(48,8);
+}
