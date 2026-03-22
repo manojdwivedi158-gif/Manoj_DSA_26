59269338eafe00e27931a70b890d2a956d9cd39a
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-23 00:24:21 +0530

    function

---
+++ c++/functions/LibraryFunction1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    cout<<sqrt(4)<<endl;
+    cout<<sqrt(14)<<endl;
+    cout<<sqrt(456)<<endl;
+    cout<<sqrt(44)<<endl;
+    cout<<sqrt(42);cout<<endl;
+    cout<<cbrt(5);
+
+}
