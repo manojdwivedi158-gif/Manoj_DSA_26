fbf31835fcc9337a19dc09d5689f464caf022cd9
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-23 23:04:02 +0530

    function

---
+++ c++/functions/LibraryFunction2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    cout<<pow(4,2)<<endl;
+    cout<<sqrt(32)<<" ";
+    cout<<cbrt(27)<<" ";
+    cout<<sqrt(5); cout<<" ";
+    cout<<cbrt(10);
+}
+
+
---
+++ c++/functions/LibraryFunction3.cpp
@@ -0,0 +1,20 @@
+// #include<iostream>
+// #include<cmath>
+// using namespace std;
+// int main(){
+//     cout<<cbrt(9);   // 2.08008
+// }
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter your number:";
+    
+    cin>>a;
+    cout<<"enter your second number:";
+    cin>>b;
+    cout<<"the square root of first number:"<< sqrt(a)<<endl;
+    cout<<"the max num "<<max(a,b);
+}
