2aaf2dd158a314d6e282c8e14bfe5cefc9d5aba1
manojdwivedi158 <manojdwivedi158@gmail.com>
2026-03-23 23:35:13 +0530

    function

---
+++ c++/functions/LibraryFunction4.cpp
@@ -0,0 +1,17 @@
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n,m;
+    cout<<"enter first number:";
+    cin>>n;
+    cout<<"enter second number:";
+    cin>>m;
+    
+    cout<<min(n,m);
+    
+    ;
+}
